b941080d625a5632d6d450d7debca04bb39c7f00
dianaqu <xiao19921201@gmail.com>
2017-04-18 19:51:31 -0700

    bonus contain system time-good for reference

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -10,18 +10,48 @@ devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
 # Install (if you haven't already) and load the `dplyr`package
+library(fueleconomy)
+install.packages('dplyr')
+library(dplyr)
 
+View(vehicles)
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+filtered <- filter(vehicles, year == 2015, make == 'Acura')
+rol.with.high.hwy <- filter(filtered, hwy == max(hwy))
+select(rol.with.high.hwy, model)
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
-
-
+best.model <- select (
+  filter(filter(vehicles, year == 2015, make == 'Acura'), hwy == max(hwy)), model
+)
 # Which Accura model has the best hwy MPG in 2015? (pipe operator)
-
+vehicles %>% filter(year == 2015, make == 'Acura') %>% filter(hwy == max(hwy)) %>% select(model)
 
 ### Bonus ###
 
 # Write 3 functions, one for each approach.  Then, 
 # Test how long it takes to perform each one 1000 times
+WithoutChaining <- function() {
+  acuras <- filter(vehicles, make == 'Acura', year == 2015)
+  best.acura <- filter(acuras, hwy == max(hwy))
+  best.model <- select(best.acura, model)
+}
+
+NestedBestModel <- function() {
+  best.model <- select(
+    filter(
+      filter(vehicles, make == 'Acura', year == 2015), hwy == max(hwy)
+    ), model
+  )
+}
+
+PipeBestModel <- function() {
+  best.model <- filter(vehicles, make == 'Acura', year == 2015) %>%
+    filter(hwy == max(hwy)) %>%
+    select(model)
+}
+
+system.time(for (i in 1:1000) WithoutChaining())
+system.time(for (i in 1:1000) NestedBestModel())
+system.time(for (i in 1:1000) PipeBestModel())
